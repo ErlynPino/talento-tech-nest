1e9e972fcbc08594dfba46b7e3d352626eda7c36
ErlynPinoTorres <98849964+ErlynPinoTorres@users.noreply.github.com>
2024-03-06 20:05:12 -0500

    schema, entity, creacion de Dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from './users.entity';
+
+
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  users(): Array<string> {
-    return this.usersService.users();
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './users.entity';
 
 @Injectable()
 export class UsersService {
-  users(): Array<string> {
-    return [];
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {} // Inyectamos el modelo de Usuarios
+
+  findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
   }
-}
+  }
+
